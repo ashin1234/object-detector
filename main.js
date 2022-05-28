28b6b0fb41c52ccd4ee62e55907d06a6a40e8304
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2022-05-27 20:32:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,25 +2,34 @@ img = "";
 status = "";
 object = [];
 function preload() {
-    img = loadImage('dog_cat.jpg');
-
 }
 function setup() {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+  
+}
+function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 function draw() {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
     if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
         for (i = 0; i < object.length; i++) {
             document.getElementById("status").innerHTML = "Status : Object Detected";
-            fill("#FA3232");
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are :"+ object.length;
+            fill(r,g,b);
             percent = floor(object[i].confidence * 100);
             text(object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
             noFill();
-            stroke("#d61d1d");
+            stroke(r,g,b);
             rect(object[i].x, object[i].y, object[i].width, object[i].height);
         }
     }
@@ -29,7 +38,7 @@ function draw() {
 function modelLoaded() {
     console.log("Model Loaded!");
     status = true;
-    objectDetector.detect(img, gotResult);
+   
 }
 function gotResult(error, results) {
     if (error) {
